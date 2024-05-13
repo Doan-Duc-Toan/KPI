2b1f743e1d60d1c1dd8750dec5b213ce9f97373d
Doan-Duc-Toan <toandtq123@gmail.com>
2024-05-14 00:01:12 +0700

    kpi manage completed

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -120,7 +120,164 @@
             </div>
         </div>
         <div id="list-kpi">
-            
+            <span class="list-kpi-title">Danh sách KPI</span>
+            <div id="kpi-list-top">
+                <div id="kpi-status">
+                    <div class="kpi-all kpi-status-item kpi-status-active">
+                        <a href="">Tất cả</a>
+                    </div>
+                    <div class="kpi-delete kpi-status-item">
+                        <a href="">Đã xóa</a>
+                    </div>
+                </div>
+                <div id="kpi-search">
+                    <form action="" class="search-kpi">
+                        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                        <input type="text" placeholder="Tìm kiếm">
+                    </form>
+                    <div class="filter">
+                        <span><i class="fa-solid fa-filter"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div id="kpi-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Tên</th>
+                            <th>Mục tiêu</th>
+                            <th>Định kỳ <i class="fa-solid fa-chevron-down"></i></th>
+                            <th>Tiến độ <i class="fa-solid fa-chevron-down"></i></th>
+                            <th>Thời hạn <i class="fa-solid fa-chevron-down"></i></th>
+                            <th>Ưu tiên <i class="fa-solid fa-chevron-down"></i></th>
+                            <th>Trạng thái</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="kpi-info"><i class="fa-solid fa-circle-info"></i></div>
+                            </td>
+                            <td>
+                                <div class="name-kpi"><span>Ngủ</span></div>
+                            </td>
+                            <td>
+                                <div>7h</div>
+                            </td>
+                            <td>
+                                <div>Ngày</div>
+                            </td>
+                            <td>
+                                <div>70%</div>
+                            </td>
+                            <td>
+                                <div>Không</div>
+                            </td>
+                            <td>
+                                <div class="text-medium">Trung bình</div>
+                            </td>
+                            <td>
+                                <div class="delete-kpi"><i class="fa-solid fa-trash-can"></i></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="kpi-info"><i class="fa-solid fa-circle-info"></i> </div>
+                            </td>
+                            <td>
+                                <div class="name-kpi"><span>AI developments</span></div>
+                            </td>
+                            <td>
+                                <div>150h</div>
+                            </td>
+                            <td>
+                                <div>Tháng</div>
+                            </td>
+                            <td>
+                                <div>60%</div>
+                            </td>
+                            <td>
+                                <div>25/08/2024</div>
+                            </td>
+                            <td>
+                                <div class="text-high">Cao</div>
+                            </td>
+                            <td>
+                                <div class="delete-kpi"><i class="fa-solid fa-trash-can"></i></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="kpi-info"><i class="fa-solid fa-circle-info"></i></div>
+                            </td>
+                            <td>
+                                <div class="name-kpi"> <span>Khóa Python 101</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div>Hoàn thành</div>
+                            </td>
+                            <td>
+                                <div>Tháng</div>
+                            </td>
+                            <td>
+                                <div>100%</div>
+                            </td>
+                            <td>
+                                <div>12/05/2024</div>
+                            </td>
+                            <td>
+                                <div class="text-medium">Trung bình</div>
+                            </td>
+                            <td>
+                                <div class="delete-kpi"><i class="fa-solid fa-trash-can"></i></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="kpi-info"><i class="fa-solid fa-circle-info"></i></div>
+                            </td>
+                            <td>
+                                <div class="name-kpi"><span>Học tiếng Nhật</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div>15h</div>
+                            </td>
+                            <td>
+                                <div>Tuần</div>
+                            </td>
+                            <td>
+                                <div>75%</div>
+                            </td>
+                            <td>
+                                <div>12/05/2024</div>
+                            </td>
+                            <td>
+                                <div class="text-low">Thấp</div>
+                            </td>
+                            <td>
+                                <div class="delete-kpi"><i class="fa-solid fa-trash-can"></i></div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div id="bot-list-kpi">
+                <div class="add-kpi">
+                    <span>+ Thêm KPI</span>
+                </div>
+                <div class="paginate">
+                    <div class="pre move"><a href=""><i class="fa-solid fa-angle-left"></i> Previous</a></div>
+                    <div class="page page-active"><a href="">1</a></div>
+                    <div class="page"><a href="">2</a></div>
+                    <div class="page"><a href="">3</a></div>
+                    <div class="page"><a href="">4</a></div>
+                    <div class="next move"><a href="">Next <i class="fa-solid fa-angle-right"></i></a></div>
+                </div>
+            </div>
+
         </div>
     </div>
     <script src="{{ asset('js/manage.js') }}"></script>
