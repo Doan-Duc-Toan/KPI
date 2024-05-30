9f9ec4bd7599cf4c38f48d45116d4543e90b8c65
Doan-Duc-Toan <toandtq123@gmail.com>
2024-05-30 16:54:20 +0700

    fix select box

--- resources/views/modal_detail_kpi.blade.php
+++ resources/views/modal_detail_kpi.blade.php
@@ -2,12 +2,25 @@
     <div class="bg-modal">
         <div class="modal-content">
             <form action="">
-                <div class="modal-content-title"><span>Chi tiết KPI</span> <div class="checkbox-wrapper-5">
-                        <div class="check">
-                            <input id="check-5" type="checkbox">
-                            <label for="check-5"></label>
-                        </div>
+                <div class="modal-content-title"><span>Chi tiết KPI</span> <div class="checkbox-wrapper-12">
+                    <div class="cbx">
+                      <input id="cbx-12" type="checkbox"/>
+                      <label for="cbx-12"></label>
+                      <svg width="15" height="14" viewbox="0 0 15 14" fill="none">
+                        <path d="M2 8.36364L6.23077 12L13 2"></path>
+                      </svg>
                     </div>
+                    <!-- Gooey-->
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                      <defs>
+                        <filter id="goo-12">
+                          <fegaussianblur in="SourceGraphic" stddeviation="4" result="blur"></fegaussianblur>
+                          <fecolormatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" result="goo-12"></fecolormatrix>
+                          <feblend in="SourceGraphic" in2="goo-12"></feblend>
+                        </filter>
+                      </defs>
+                    </svg>
+                  </div>
                 </div>
 
                 <div id="kpi-name" class="form-create-kpi-group flex-full">
